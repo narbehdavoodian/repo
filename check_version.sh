85ace5b9eeb96f5740f5ebaad0c46582625a0c83
narbehdavoodian <narbeh.davoodian@deliveryhero.com>
2022-11-11 23:19:54 +0100

    commit

--- check_version.sh
+++ check_version.sh
@@ -1,3 +1,4 @@
 software=$1
 curl -s https://raw.githubusercontent.com/narbehdavoodian/repo/master/versions.json -o versions.json
 jq -r ".software.`echo $software`[]" versions.json
+rm versions.json
