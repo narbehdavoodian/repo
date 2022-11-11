3ccf045db061407be1a0d10259fba921260a0ccc
narbehdavoodian <narbeh.davoodian@deliveryhero.com>
2022-11-11 23:22:52 +0100

    commit

--- check_version.sh
+++ check_version.sh
@@ -2,3 +2,7 @@ software=$1
 curl -s https://raw.githubusercontent.com/narbehdavoodian/repo/master/versions.json -o versions.json
 jq -r ".software.`echo $software`[]" versions.json
 rm versions.json
+
+
+## this is how you get the version from json
+# version=$(curl -s https://raw.githubusercontent.com/narbehdavoodian/repo/master/check_version.sh | bash -s -- "onenote")
