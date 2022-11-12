ad6b6d43c81431272fb108a924aa9af38a5c149c
narbehdavoodian <narbeh.davoodian@deliveryhero.com>
2022-11-12 13:54:00 +0100

    commit

--- check_version.ps1
+++ check_version.ps1
@@ -1,14 +1,13 @@
 param
 (
-    [string]$software=""
+    [string]$software
 )
 
 $filepath="https://raw.githubusercontent.com/narbehdavoodian/repo/master/versions.json"
+$req=Invoke-RestMethod $filepath
 
-$req=Invoke-WebRequest  $filepath
-$json = convertfrom-json $req.Content
-
-return $json.software.$software.version
+return $req.software.$software.version
 
 ## @narbehdavoodian this is how you get the version from json
-# $version= Invoke-WebRequest https://raw.githubusercontent.com/narbehdavoodian/repo/master/check_version.ps1 -software "skype"
+#  Invoke-RestMethod https://raw.githubusercontent.com/narbehdavoodian/repo/master/check_version.ps1 -OutFile check_version.ps1                   
+#  $version=(./check_version.ps1 -software "skype")                                                                  
