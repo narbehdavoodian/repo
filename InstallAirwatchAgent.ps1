44f1e9e772c9df49d23f896131c42c0de2cb75e7
narbehdavoodian <narbeh.davoodian@deliveryhero.com>
2022-11-21 12:02:48 +0100

    Install Intelligent Hub 1.2

--- InstallAirwatchAgent.ps1
+++ InstallAirwatchAgent.ps1
@@ -3,8 +3,8 @@ $logfile="C:\WSONEInstallLog.txt"
 $link="https://packages.vmware.com/wsone/AirwatchAgent.msi"
 
 "$(Get-date); Downloading Intelligent Hub $link" | Out-File $logfile -Append
-Invoke-RestMethod -Method Get -URI $link -OutFile C:\AirwatchAgent.msi
-
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($link, "C:\AirwatchAgent.msi")
 Unblock-File C:\AirwatchAgent.msi
 
 "$(Get-date); Enrolling computer" | Out-File $logfile -Append
